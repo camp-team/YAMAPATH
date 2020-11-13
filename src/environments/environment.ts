06d9274d6a2a8e35c778fc097ab6a13e22577584
hide-a1 <hide210g@gmail.com>
2020-11-13 13:22:40 +0900

    angular fire init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAu0Hk3yMvcsmoSCmq-D2wkFbMrw-20a48',
+    authDomain: 'yamapath-dev-40f13.firebaseapp.com',
+    databaseURL: 'https://yamapath-dev-40f13.firebaseio.com',
+    projectId: 'yamapath-dev-40f13',
+    storageBucket: 'yamapath-dev-40f13.appspot.com',
+    messagingSenderId: '729915230166',
+    appId: '1:729915230166:web:f6d89b43491108c1484677',
+    measurementId: 'G-L9YMRVQFGT'
+  },
 };
 
 /*
