7ba4f5d887b90d210e201b340e984d4c6d070653
hide-a1 <hide210g@gmail.com>
2020-11-13 15:47:18 +0900

    feat(header): ヘッダー仮作成

--- src/app/main-shell/main-shell.module.ts
+++ src/app/main-shell/main-shell.module.ts
@@ -5,13 +5,15 @@ import { MainShellRoutingModule } from './main-shell-routing.module';
 import { MainShellComponent } from './main-shell/main-shell.component';
 import { HeaderComponent } from './header/header.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [MainShellComponent, HeaderComponent, ToolbarComponent],
   imports: [
     CommonModule,
-    MainShellRoutingModule
+    MainShellRoutingModule,
+    MatIconModule,
   ]
 })
 export class MainShellModule { }
