105ce292f5dff7eb2fe6b25a5eab7956dc2f1c27
hide-a1 <hide210g@gmail.com>
2020-11-19 14:55:17 +0900

    feat(home): ホーム画面仮作成

--- src/app/main-shell/main-shell-routing.module.ts
+++ src/app/main-shell/main-shell-routing.module.ts
@@ -6,6 +6,14 @@ const routes: Routes = [
   {
     path: '',
     component: MainShellComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
+      },
+    ]
   }
 ];
 
