8774c4ad0ae950d65f2b8ab7aa10cb8ed250aca8
hide-a1 <hide210g@gmail.com>
2020-11-20 16:26:31 +0900

    feat(post): 記事のinterface作成

---
+++ src/app/interfaces/post.ts
@@ -0,0 +1,10 @@
+export interface Post {
+  postId: string;
+  category: string;
+  text: string;
+  authorUid: string;
+  likedCount: number;
+  position: google.maps.LatLngLiteral;
+  createdAt: number;
+  isPublic: boolean;
+}
