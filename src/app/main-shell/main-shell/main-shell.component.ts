9f80926da9c3348adab0693b142fe006de243321
hide-a1 <hide210g@gmail.com>
2020-12-22 11:58:22 +0900

    feat: 認証情報を監視してページ遷移させる

--- src/app/main-shell/main-shell/main-shell.component.ts
+++ src/app/main-shell/main-shell/main-shell.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-main-shell',
   templateUrl: './main-shell.component.html',
-  styleUrls: ['./main-shell.component.scss']
+  styleUrls: ['./main-shell.component.scss'],
 })
-export class MainShellComponent implements OnInit {
+export class MainShellComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.subscription = this.authService.user$.subscribe((user) => {
+      if (!user) {
+        this.router.navigate(['/welcome']);
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/welcome/welcome/welcome.component.ts
+++ src/app/welcome/welcome/welcome.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent implements OnInit {
-  constructor() {}
+export class WelcomeComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
 
-  ngOnInit(): void {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
